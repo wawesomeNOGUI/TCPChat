86fc6b7d58b0ce4ba3b2f72893038dcf2ed44501
Will Forcey <wsforc3y@gmail.com>
2021-07-05 16:58:11 -0400

    Add files via upload

--- clinetTCP.go
+++ clinetTCP.go
@@ -3,57 +3,57 @@
 package main
 
 import (
-    "bufio"
-    "io"
-    "fmt"
-    "net"
-    "os"
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 func read(conn net.Conn) {
-  reader := bufio.NewReader(conn)
-  b := make([]byte, 512)
+	reader := bufio.NewReader(conn)
+	b := make([]byte, 512)
 
-  for {
-    _, err := reader.Read(b)
-    if err == io.EOF {
-      fmt.Println("Disconnected From Server")
-      panic(err)
-    } else if err != nil {
-      fmt.Println(err)
-      panic(err)
-    }
-    fmt.Println("Echo From Sever: " + string(b))
-  }
+	for {
+		_, err := reader.Read(b)
+		if err == io.EOF {
+			fmt.Println("Disconnected From Server")
+			panic(err)
+		} else if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+		fmt.Println("Echo From Sever: " + string(b))
+	}
 
 }
 
 func write(conn net.Conn) {
-  writer := bufio.NewWriter(conn)
-  stdin := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(conn)
+	stdin := bufio.NewReader(os.Stdin)
 
-  for {
-    userInput, _ := stdin.ReadString('\n')
-    writer.Write([]byte(userInput))
-    writer.Flush()
-  }
+	for {
+		userInput, _ := stdin.ReadString('\n')
+		writer.Write([]byte(userInput))
+		writer.Flush()
+	}
 }
 
 func main() {
-    fmt.Println("Where would you like to connect? e.g. 127.0.0.1:6000")
-    var ip string
-    fmt.Scanln(&ip)
+	fmt.Println("Where would you like to connect? e.g. 127.0.0.1:6000")
+	var ip string
+	fmt.Scanln(&ip)
 
-    conn, err := net.Dial("tcp", ip)
-    if err != nil {
-        fmt.Println(err)
-        //os.Exit(1)
-    }
-    fmt.Println(conn.LocalAddr().String())
-    fmt.Println("Type and press enter to send messages!")
+	conn, err := net.Dial("tcp", ip)
+	if err != nil {
+		fmt.Println(err)
+		//os.Exit(1)
+	}
+	fmt.Println(conn.LocalAddr().String())
+	fmt.Println("Type and press enter to send messages!")
 
-    go read(conn)
-    go write(conn)
+	go read(conn)
+	go write(conn)
 
-    select{} // Block forever
+	select {} // Block forever
 }
